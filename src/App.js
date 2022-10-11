9191789d141aac8872b0ba43946436fa1a3d6224
AdiPat <contact.adityapatange@gmail.com>
2022-10-11 19:43:09 +0530

    app: add home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ProSidebarProvider } from "react-pro-sidebar";
+import { HomePage } from "./pages";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ProSidebarProvider>
+      <div className="App">
+        <HomePage />
+      </div>
+    </ProSidebarProvider>
   );
 }
 
---
+++ src/pages/home-page.js
@@ -0,0 +1,34 @@
+import { SidebarLayout } from "../components";
+
+function HomePage() {
+  return (
+    <>
+      <SidebarLayout>
+        <h1>Home Page</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a
+          felis ut felis ultricies rhoncus. Nunc gravida sapien porta, rutrum
+          diam id, consequat augue. Curabitur sit amet sapien ultrices,
+          porttitor nibh quis, placerat enim. Sed arcu lectus, cursus quis lorem
+          ac, facilisis imperdiet augue. Suspendisse sit amet nibh ac mi
+          sagittis efficitur. Proin euismod, metus sit amet mattis sagittis,
+          eros orci auctor augue, sit amet mollis nibh risus quis nunc. Aenean
+          laoreet risus et viverra sagittis. Mauris ultrices nec felis ac
+          suscipit. Pellentesque in justo justo.
+        </p>
+        <p>
+          Proin sed ornare mauris. Phasellus non lectus ultricies, luctus ante
+          vitae, pretium lectus. Suspendisse hendrerit ante vel magna cursus
+          pharetra. Suspendisse volutpat lacinia fringilla. Donec vitae arcu sed
+          urna maximus ultricies. Pellentesque dictum non dui vitae facilisis.
+          Aliquam congue vitae lacus ut commodo. Vivamus dictum enim congue
+          massa venenatis imperdiet. Morbi quis elit id ex porta blandit. Donec
+          varius, ex eget laoreet mattis, justo libero maximus turpis, et
+          porttitor tortor nisi non odio.
+        </p>
+      </SidebarLayout>
+    </>
+  );
+}
+
+export default HomePage;
