ebf42c036415274147bdd29e101fe84d80ff7a31
AdiPat <contact.adityapatange@gmail.com>
2022-10-16 23:16:23 +0530

    coin-info: add loading spinner and chartjs

--- src/pages/coin-info-page.js
+++ src/pages/coin-info-page.js
@@ -6,6 +6,7 @@ import { CgApi } from "../api/coingecko";
 import { parseGenesisDate } from "../utils";
 
 function CoinInfoPage() {
+  const [isLoading, setIsLoading] = useState(true);
   const [coinInfo, setCoinInfo] = useState(null);
   const { coinId } = useParams();
 
@@ -22,6 +23,11 @@ function CoinInfoPage() {
   const genesisDate = parseGenesisDate(genesis_date);
 
   useEffect(() => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1500);
+
     CgApi.coinById({ coinId }).then((coinInfo) => {
       setCoinInfo(coinInfo);
     });
@@ -30,51 +36,63 @@ function CoinInfoPage() {
   return (
     <SidebarLayout>
       <div className="coin-info-container">
-        <div className="coin-info-header pt-4">
-          <Image src={image?.large} width={200} height={200} />
-          <div className="coin-info-header-text mx-4">
-            <h2>
-              {name} <Badge className="symbol-badge">{symbol}</Badge>{" "}
-            </h2>
-            <p>
-              <strong>Genesis Date: &nbsp;</strong>
-              {genesisDate}
-            </p>
-            <div dangerouslySetInnerHTML={{ __html: description?.en }}></div>
+        {isLoading ? (
+          <div className="centered-spinner">
+            <Spinner animation="border" variant="primary" />
           </div>
-        </div>
-        <div className="coin-info-stats">
-          <Table bordered hover>
-            <thead>
-              <tr>
-                <th>Parameter</th>
-                <th>Value</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Current Price</td>
-                <td>${marketData?.current_price?.usd}</td>
-              </tr>
-              <tr>
-                <td>Market Cap</td>
-                <td>${marketData?.market_cap?.usd}</td>
-              </tr>
-              <tr>
-                <td>Total Volume</td>
-                <td>{marketData?.total_volume?.usd}</td>
-              </tr>
-              <tr>
-                <td>24H High</td>
-                <td>${marketData?.high_24h?.usd}</td>
-              </tr>
-              <tr>
-                <td>24H Low</td>
-                <td>${marketData?.low_24h?.usd}</td>
-              </tr>
-            </tbody>
-          </Table>
-        </div>
+        ) : null}
+        {!isLoading && (
+          <div className="coin-info-header pt-4">
+            <Image src={image?.large} width={200} height={200} />
+            <div className="coin-info-header-text mx-4">
+              <h2>
+                {name}{" "}
+                <Badge className="symbol-badge" bg="warning">
+                  {symbol}
+                </Badge>{" "}
+              </h2>
+              <p>
+                <strong>Genesis Date: &nbsp;</strong>
+                {genesisDate}
+              </p>
+              <div dangerouslySetInnerHTML={{ __html: description?.en }}></div>
+            </div>
+          </div>
+        )}
+        {!isLoading && (
+          <div className="coin-info-stats">
+            <Table bordered hover>
+              <thead>
+                <tr>
+                  <th>Parameter</th>
+                  <th>Value</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Current Price</td>
+                  <td>${marketData?.current_price?.usd}</td>
+                </tr>
+                <tr>
+                  <td>Market Cap</td>
+                  <td>${marketData?.market_cap?.usd}</td>
+                </tr>
+                <tr>
+                  <td>Total Volume</td>
+                  <td>{marketData?.total_volume?.usd}</td>
+                </tr>
+                <tr>
+                  <td>24H High</td>
+                  <td>${marketData?.high_24h?.usd}</td>
+                </tr>
+                <tr>
+                  <td>24H Low</td>
+                  <td>${marketData?.low_24h?.usd}</td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
+        )}
       </div>
     </SidebarLayout>
   );
--- src/utils/index.js
+++ src/utils/index.js
@@ -2,7 +2,12 @@ import moment from "moment";
 
 const parseGenesisDate = (dateStr) => {
   const dateMoment = moment(dateStr, "YYYY-MM-DD");
-  return dateMoment.format("Do MMMM YYYY");
+
+  if (dateMoment.isValid()) {
+    return dateMoment.format("Do MMMM YYYY");
+  }
+
+  return "NA";
 };
 
 export { parseGenesisDate };
